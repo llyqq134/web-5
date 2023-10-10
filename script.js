e258a5ade8d05fcbf32f1c0ed5c14563e33e3ed7
llyqq134 <pinklywmp@gmail.com>
2023-10-10 21:38:57 +0300

    some

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function onClickButton(el) {
-    let field1 = document.getElementsByName("field1");
-    let field2 = document.getElementsByName("field2");
-    let r = document.getElementById("result");
-    r.innerHTML = field1[0].value + field2[0].value;
+    let result = document.getElementsByName("result");
+    result.innerHTML = "ewq";
 }
